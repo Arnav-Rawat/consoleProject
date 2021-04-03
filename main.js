b14935cc7dd1b14bd281af9b003f0c01ec79d849
Arnav-Rawat <77447759+Arnav-Rawat@users.noreply.github.com>
2021-04-03 15:58:31 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,28 @@
+console.log("The Console Project")
+var first_name = "Arnav ";
+var last_name = "Rawat";
+
+var full_name = first_name.concat(last_name);
+
+console.log(full_name);
+
+var a = 10;
+var b = 20;
+
+var add= a + b;
+
+console.log(add);
+
+var sub = b - a;
+
+console.log(sub);
+
+var mul = a * b;
+
+console.log(mul);
+
+var div = b / a;
+
+console.log(div);
+
+console.log("thank you");
